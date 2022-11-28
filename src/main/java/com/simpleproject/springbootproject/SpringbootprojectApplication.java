c3a5705dd587affb40f9f3d50fd0b7a08407400e
Chibix anastasiya <chibisovanastena@gmail.com>
2022-11-28 18:49:42 +0300

    change

--- src/main/java/com/simpleproject/springbootproject/SpringbootprojectApplication.java
+++ src/main/java/com/simpleproject/springbootproject/SpringbootprojectApplication.java
@@ -2,13 +2,26 @@ package com.simpleproject.springbootproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringbootprojectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootprojectApplication.class, args);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
+
+
     }
 
 }
